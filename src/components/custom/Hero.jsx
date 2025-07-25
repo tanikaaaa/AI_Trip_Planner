42d9f0742ac0641b858b8b1730616888398ad2d1
tanika goyal <2023ucp1847@mnit.ac.in>
2025-07-26 00:33:54 +0530

    FINAL CHANGES

--- src/components/custom/Hero.jsx
+++ src/components/custom/Hero.jsx
@@ -7,7 +7,7 @@ function Hero() {
     <div className='flex flex-col items-center mx-56 gap-9'>
       <h1 className='font-extrabold text-[50px] text-center mt-10'><span className='text-[#f56551]'>Discover Your Next Adventure with AI: </span>Personalized Itineraries at Your Fingertips</h1>
 
-      <p className='text-xl text-gray-500 text-center'>Your personal trip planner and travel curator, creating custom itineraries tailored to your interests and budget.</p>
+      <p className='text-xl text-gray-900 text-center'>From dreamy escapes to weekend getaways, get smart, personalized itineraries—instantly, all in one place</p>
 
       <Link to={'/create-trip'}><Button>Get Started, It's free</Button></Link>
 
